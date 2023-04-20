873866080887840833f8dbedb0418149eff9ed18
Oleksii Khomiakov <alex.xag1987@gmail.com>
2023-04-20 23:27:19 +0300

    webpack is ready

--- src/index.js
+++
@@ -1,3 +0,0 @@
-import name from "./module-1";
-
-console.log("This is index.js");
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -6,6 +8,29 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        // test: /\.css$/i,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new MiniCssExtractPlugin({ filename: "styles.css" }),
+  ],
   devServer: {
     port: 8080,
   },
